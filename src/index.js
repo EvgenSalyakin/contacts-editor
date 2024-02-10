fe9a2f89f4b96ab7538369a3d78d77905140022c
EvgenSalyakin <e.salyakin@gmail.com>
2024-02-10 17:54:35 +0100

    init

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { App, Header, Footer } from "components";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header title="React template" />
+    <Header title="Editor for contacts" />
     <App />
     <Footer />
   </React.StrictMode>
